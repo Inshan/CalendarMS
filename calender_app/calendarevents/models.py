3cdbda0e47ac5c0b55afac5021b18d4937e9f11d
InshanBhattarai <inshanbhattarai2045@gmail.com>
2024-05-28 15:16:44 +0545

    Timezone changed

--- calender_app/calendarevents/models.py
+++ calender_app/calendarevents/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 import pytz
 
+TIMEZONE_CHOICES = [(tz, tz) for tz in pytz.all_timezones]
+
 class Event(models.Model):
     title = models.CharField(max_length=200, null=True)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    timezone = models.CharField(max_length=50)
-
+    timezone = models.CharField(max_length=63, choices=TIMEZONE_CHOICES, default='UTC')
+    
     def __str__(self):
         return self.title
