91beae2e732484274e635e183bd9b52987c6d4f4
InshanBhattarai <inshanbhattarai2045@gmail.com>
2024-05-28 15:18:27 +0545

    No create-func used

--- calender_app/calendarevents/api_views.py
+++ calender_app/calendarevents/api_views.py
@@ -15,12 +15,12 @@ class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                {"message": "Event form data received"},
-                status=status.HTTP_201_CREATED,
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(
+    #             {"message": "Event form data received"},
+    #             status=status.HTTP_201_CREATED,
+    #         )
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
