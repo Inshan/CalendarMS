31b90950cf765b456c9d4d9c4234fc6ffa3d18f4
InshanBhattarai <inshanbhattarai2045@gmail.com>
2024-05-28 15:36:04 +0545

    OnlyBackend Files

--- calender_app/calendarevents/admin.py
+++ calender_app/calendarevents/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Event
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title','description','start_time','end_time', 'timezone')
+    search_fields = ('timezone',)
+
+admin.site.register(Event, EventAdmin)
